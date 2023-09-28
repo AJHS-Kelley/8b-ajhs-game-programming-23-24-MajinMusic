4b726908ba5f3b04ce710180e2a6dcde6a6fdfe3
MajinMusic <jacobhyper101@gmail.com>
2023-09-28 14:45:15 -0400

    [Python] Collections 0.4a
    Added .sort() method examples.

--- gameProgramming/computer_science_exercises/02_Collections/collections.py
+++ gameProgramming/computer_science_exercises/02_Collections/collections.py
@@ -102,3 +102,15 @@ classGPA = [3.14, 2.25, 1.74, 1.99, 0.99, 4.25]
 print(f"There are {len(breakfastFoods)} items in the breakfastFoods list.")
 print(f"There are {len(testScores)} items in the testScores list.")
 print(f"There are {len(classGPA)} items in the classGPA list.")
+
+# List Methods -- Functions for working with lists.
+# Sorting Lists -- Aplhanumerical -- Ascending -- Capital Letters before Lower Cases Letters
+print(f"The original breakfastFoods list is {breakfastFoods}.")
+breakfastFoods.sort()
+print(f"The sorted breakfastFoods list is {breakfastFoods}.")
+print(f"The original breakfastFoods list is {testScores}.")
+testScores.sort()
+print(f"The sorted breakfastFoods list is {testScores}.")
+print(f"The original breakfastFoods list is {classGPA}.")
+classGPA.sort()
+print(f"The sorted breakfastFoods list is {classGPA}.")
