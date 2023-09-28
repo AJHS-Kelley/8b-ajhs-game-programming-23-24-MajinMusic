261b8604a5cd861076e77d5d6195aa496bee5f0e
MajinMusic <jacobhyper101@gmail.com>
2023-09-28 14:40:31 -0400

    [Python] Collections v0.3a
    Added code to determine length of a list.

--- gameProgramming/computer_science_exercises/02_Collections/collections.py
+++ gameProgramming/computer_science_exercises/02_Collections/collections.py
@@ -1,4 +1,4 @@
-# Collections Examples, Jacob Desha, v0.2b
+# Collections Examples, Jacob Desha, v0.3a
 
 # LIST -- ORDERED, CHANGEAVLE, ALLOWS DUPLICATE VALUES\
 breakfastFoods = ["Bacon", "Eggs", "Cereal", "Pancakes", "Milk"]
@@ -83,18 +83,22 @@ classGPA = [3.14, 2.25, 1.74, 1.99, 0.99, 4.25]
 #print(classGPA)
 
 # To delete using the index value we use .pop()
-breakfastFoods.pop(4)
-print(breakfastFoods)
-testScores.pop(4)
-print(testScores)
-classGPA.pop(4)
-print(classGPA)
+#breakfastFoods.pop(4)
+#print(breakfastFoods)
+#testScores.pop(4)
+#print(testScores)
+#classGPA.pop(4)
+#print(classGPA)
 
 # PAUSE - WYOC -- .pop() the 2nd element from each list.  .remove() any item from the list.
-breakfastFoods.remove("Eggs")
-print(breakfastFoods)
-testScores.remove(25)
-print(testScores)
-classGPA.remove(1.74)
-print(classGPA)
+#breakfastFoods.remove("Eggs")
+#print(breakfastFoods)
+#testScores.remove(25)
+#print(testScores)
+#classGPA.remove(1.74)
+#print(classGPA)
 
+# Determining List Length
+print(f"There are {len(breakfastFoods)} items in the breakfastFoods list.")
+print(f"There are {len(testScores)} items in the testScores list.")
+print(f"There are {len(classGPA)} items in the classGPA list.")
