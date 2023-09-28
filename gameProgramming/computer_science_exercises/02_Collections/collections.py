da26d38abd21a744d9b80c8ce1a143707fb62eab
MajinMusic <jacobhyper101@gmail.com>
2023-09-28 13:59:16 -0400

    v0.1a
    Declared And Defined Three Lists.

--- gameProgramming/computer_science_exercises/02_Collections/collections.py
+++ gameProgramming/computer_science_exercises/02_Collections/collections.py
@@ -1,1 +1,11 @@
-# Collections Examples, Jacob Desha, v0.0
+# Collections Examples, Jacob Desha, v0.1a
+
+# LIST -- ORDERED, CHANGEAVLE, ALLOWS DUPLICATE VALUES\
+breakfastFoods = ["Bacon", "Eggs", "Cereal", "Pancakes", "Milk"]
+# Each itom on the list is known as an ELEMENT.
+# The position in the list for each is the INDEX.
+# The element "BACON" is at index 0.
+# Python Only: index -1 is the last itom on the list.
+testScores = [95, 100, 25, 15, 27, 35]
+classGPA = [3.14, 2.25, 1.74, 1.99, 0.99, 4.25]
+
