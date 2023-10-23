366be11fb792d3cf6a07aefd0f47e7175952930e
MajinMusic <jacobhyper101@gmail.com>
2023-10-23 14:52:14 -0400

    Created A Dice Roller

--- gameProgramming/03b_functions/functions.py
+++ gameProgramming/03b_functions/functions.py
@@ -1,3 +1,4 @@
+import random
 # FUNCTION -- a named piece of code that can be reused easily.
 # FUNCTION SIGNATURE -- Syntax for creating aexm new function.
 def exampleFunctionA(): # NO PARAMETERS
@@ -15,6 +16,30 @@ def exampleFunctionB(num, count): # PARAMETERS
 # IF A FUNCTION REQUIRES PARAMETERS, YOUR CODE WILL CRASH WITHOUT THEM!
 
 # RUNNING A FUNCTION IS KNOWN AS CALLING THE FUNCTION!
+# exampleFunctionA()
+# exampleFunctionB(5, 0)
 
-exampleFunctionA()
-exampleFunctionB(5, 0)
+def rollDice(numDice, sizeDice):
+    numRolled = 0
+    sum = 0
+    while numRolled < numDice:
+        roll = random.randint(1, sizeDice)
+        sum += roll
+        print(f"Roll: {roll}\n")
+        print(f"Sum: {sum}\n")
+        numRolled += 1
+    return sum
+
+rollDice(5, 7)
+rollDice(6, 10)
+
+strengthPlayer = rollDice(3, 6)
+dexterityPlayer = rollDice(3, 6)
+wisdomPlayer = rollDice(3, 6)
+
+print(strengthPlayer)
+print(dexterityPlayer)
+print(wisdomPlayer)
+
+
+
