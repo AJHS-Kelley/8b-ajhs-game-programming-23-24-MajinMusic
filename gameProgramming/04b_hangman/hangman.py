83d624b387f345ae0f2546eb8221a38fdd1003f6
MajinMusic <jacobhyper101@gmail.com>
2023-10-31 13:57:40 -0400

    v0.1
    Made word list and used .split

--- gameProgramming/04b_hangman/hangman.py
+++ gameProgramming/04b_hangman/hangman.py
@@ -1,1 +1,6 @@
-# Hangman Game by Jacob Desha, v0.0
+# Hangman Game by Jacob Desha, v0.1
+
+words = 'friend, great, world, king, dog, queen, fish, tiger, seven, apple, intervention, coordination, intellectual, xylophone, containment, continental, satelite, burdensome, enlightened, economy, philanthropist, appendicitis, pseudoscience, creationism, geriatrics, xenomophobic, hippopotomonstrosesquippedaliophobia, neuroscopic, incomprehensible, inconsequential'.split()
+
+print (words)
+
