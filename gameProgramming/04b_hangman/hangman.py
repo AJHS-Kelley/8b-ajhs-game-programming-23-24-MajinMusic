608e3ccd15bbd7f05f68a2d8057005576ade54d0
MajinMusic <jacobhyper101@gmail.com>
2023-10-31 14:14:55 -0400

    v0.2
    Made a Hangman board.

--- gameProgramming/04b_hangman/hangman.py
+++ gameProgramming/04b_hangman/hangman.py
@@ -2,5 +2,40 @@
 
 words = 'friend, great, world, king, dog, queen, fish, tiger, seven, apple, intervention, coordination, intellectual, xylophone, containment, continental, satelite, burdensome, enlightened, economy, philanthropist, appendicitis, pseudoscience, creationism, geriatrics, xenomophobic, hippopotomonstrosesquippedaliophobia, neuroscopic, incomprehensible, inconsequential'.split()
 
-print (words)
-
+# VARIABLE_NAMES in ALL-CAPS ARE CONSTANTS AND NOT MEANT TO CHANGE!
+HANGMAN_BOARD = ['''
+    +---+
+        |
+        |
+        |
+     =======''', '''
+    +---+
+    o   |
+        |
+        |
+     =======''', '''
+    +---+
+    o   |
+    |   |
+        |
+     =======''', '''
+    +---+
+    o   |
+   /|   |
+        |
+     =======''', '''
+    +---+
+    o   |
+   /|\  |
+        |
+     =======''', '''
+    +---+
+    o   |
+   /|\  |
+   /    |
+     =======''', '''
+    +---+
+    o   |
+   /|\  |
+   / \  |
+     =======''']
