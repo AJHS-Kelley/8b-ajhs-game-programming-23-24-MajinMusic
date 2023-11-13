ccf6bb8d5ea04303c07b6d1b007f1fa1625e5321
MajinMusic <jacobhyper101@gmail.com>
2023-11-13 13:50:49 -0500

    exampleGameFunctions v0.1
    Created placeholder functions.

---
+++ gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
@@ -0,0 +1,14 @@
+# Example Game Functions Project, Jacob Desha, v0.1
+import random
+
+def functionOne():
+    pass
+
+def functionTwo(param1):
+    pass
+
+def functionThree(param1 = "Default Value"):
+    print(param1)
+
+functionThree("Test")
+functionThree()
