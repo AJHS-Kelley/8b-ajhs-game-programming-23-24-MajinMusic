1cbd75df034ec4f5f3f1449448351b239b79ad34
MajinMusic <jacobhyper101@gmail.com>
2023-12-14 13:41:02 -0500

    dice v0.1
    created display function

--- gameProgramming/modules/dice.py
+++ gameProgramming/modules/dice.py
@@ -1,7 +1,16 @@
-# Dice Rolling Module by Jacob Desha, v0.0
+# Dice Rolling Module by Jacob Desha, v0.1
 import random 
 
-def rollDice(numDice, sizeDice):
+def roll(numDice, sizeDice):
+    numRolled = 0
+    sum = 0
+    while numRolled < numDice:
+        roll = random.randint(1, sizeDice)
+        sum += roll
+        numRolled += 1
+    return sum # return will IMMEDIATELY exit a loop, function, if/elif/else block.
+
+def display(numDice, sizeDice):
     numRolled = 0
     sum = 0
     while numRolled < numDice:
@@ -11,3 +20,4 @@ def rollDice(numDice, sizeDice):
         print(f"Sum: {sum}\n")
         numRolled += 1
     return sum # return will IMMEDIATELY exit a loop, function, if/elif/else block.
+
