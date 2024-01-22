de2b53583bd7080edbc1359e679dbaa5a8c1dada
MajinMusic <jacobhyper101@gmail.com>
2024-01-22 13:59:31 -0500

    Number Slider v0.1
    Set Boundaries.

--- gameProgramming/07_numberSlider/numberSlider.py
+++ gameProgramming/07_numberSlider/numberSlider.py
@@ -1,1 +1,18 @@
-# Number SLider by Jacob Desha, based on a project by Al Sweigart, v0.0
+# Number SLider by Jacob Desha, based on a project by Al Sweigart, v0.1
+
+import sys, random, pygame
+# sys module provides access to system resources (i.e. Operating System Commands) 
+
+from pygame.locals import *
+# Allows us to call functions from pygame using just the function name instead of module.function()
+# Example: We can use draw() instead of pygame.draw()
+
+# Constants for Game Board
+BOARDWIDTH = 4 # Columns
+BOARDHEIGHT = 4 # Rows
+TILESIZE = 80 # MEASURED IN PIXELS
+WINDOWWIDTH = 640 # MEASURED IN PIXELS
+WINDOWHEIGHT = 480 # MEASURED IN PIXELS
+FPS = 30 # This is a maximum value! Won't make a slow computer run faster.
+
+BLANK = None
