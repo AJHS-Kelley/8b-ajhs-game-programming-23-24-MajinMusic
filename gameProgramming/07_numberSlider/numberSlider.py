5f748504b86db14a2e65882d2b71da21d1d0c733
MajinMusic <jacobhyper101@gmail.com>
2024-01-22 14:04:04 -0500

    numberSlider v0.2
    Set up colors.

--- gameProgramming/07_numberSlider/numberSlider.py
+++ gameProgramming/07_numberSlider/numberSlider.py
@@ -1,4 +1,4 @@
-# Number SLider by Jacob Desha, based on a project by Al Sweigart, v0.1
+# Number SLider by Jacob Desha, based on a project by Al Sweigart, v0.2
 
 import sys, random, pygame
 # sys module provides access to system resources (i.e. Operating System Commands) 
@@ -14,5 +14,11 @@ TILESIZE = 80 # MEASURED IN PIXELS
 WINDOWWIDTH = 640 # MEASURED IN PIXELS
 WINDOWHEIGHT = 480 # MEASURED IN PIXELS
 FPS = 30 # This is a maximum value! Won't make a slow computer run faster.
-
 BLANK = None
+
+# COLOR VALUES in (R, G, B) format.
+# 0 = No value, 255 = Max Value
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BRIGHTBLUE = (0, 50, 255)
+DARKTURQUOISE = (3, 54, 73)
