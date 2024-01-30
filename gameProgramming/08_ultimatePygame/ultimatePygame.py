f7f06f9a5371ae4eef5952296c96035958054005
MajinMusic <jacobhyper101@gmail.com>
2024-01-30 14:04:06 -0500

    The Ultimate Pygame v0.1
    Made it so screen can now close and made the title for the window.

--- gameProgramming/08_ultimatePygame/ultimatePygame.py
+++ gameProgramming/08_ultimatePygame/ultimatePygame.py
@@ -1,6 +1,17 @@
 # The Ultimate Pygame by Jacob Desha v0.0
 
 import pygame
+from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode
+screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption('Game')
+
+while True: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    # Draw all our elements
+    # Update everything
+    pygame.display.update()
