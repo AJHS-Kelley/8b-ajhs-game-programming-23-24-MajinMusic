d6126345395a77467e708775d93cdbbd676d00fc
MajinMusic <jacobhyper101@gmail.com>
2024-02-01 14:27:02 -0500

    ultPygame v0.4
    Added animated snail, fixed ground_surface to screen, and added text_surface.

--- gameProgramming/08_ultimatePygame/ultimatePygame.py
+++ gameProgramming/08_ultimatePygame/ultimatePygame.py
@@ -1,4 +1,4 @@
-# The Ultimate Pygame by Jacob Desha v0.3
+# The Ultimate Pygame by Jacob Desha v0.4
 
 import pygame
 from sys import exit
@@ -7,10 +7,15 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Game')
 clock = pygame.time.Clock()
+test_font = pygame.font.Font(None, 50) #(Font type, font size)
+
 
 sky_surface = pygame.image.load("C:\8b-ajhs-game-programming-23-24-MajinMusic\img\sky.jpg")
 ground_surface = pygame.image.load("C:\8b-ajhs-game-programming-23-24-MajinMusic\img\ground.jpg")
+text_surface = test_font.render('My game', False, 'Blue') #(text, AA, color)
 
+snail_surface = pygame.image.load("C:\8b-ajhs-game-programming-23-24-MajinMusic\img\download.png")
+snail_x_pos = 600
 
 while True: 
     for event in pygame.event.get():
@@ -19,7 +24,11 @@ while True:
             exit()
     # Draw all our elements
     screen.blit(sky_surface,(100,0))
-    screen.blit(ground_surface, (0,300))
+    screen.blit(ground_surface, (50,300))
+    screen.blit(text_surface, (300, 50))
+    snail_x_pos -= 4
+    if snail_x_pos < -100: snail_x_pos = 800
+    screen.blit(snail_surface, (snail_x_pos, 200))
     # Update everything
     pygame.display.update()
     clock.tick(60)
