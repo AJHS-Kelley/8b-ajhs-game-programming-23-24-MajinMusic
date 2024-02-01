a3dbe0b5417ca549e88e1fa81d8a2e41576b697d
MajinMusic <jacobhyper101@gmail.com>
2024-02-01 13:43:14 -0500

    ultPygame v0.3
    created ground and sky surfaces.

--- gameProgramming/08_ultimatePygame/ultimatePygame.py
+++ gameProgramming/08_ultimatePygame/ultimatePygame.py
@@ -1,4 +1,4 @@
-# The Ultimate Pygame by Jacob Desha v0.2
+# The Ultimate Pygame by Jacob Desha v0.3
 
 import pygame
 from sys import exit
@@ -8,12 +8,18 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Game')
 clock = pygame.time.Clock()
 
+sky_surface = pygame.image.load("C:\8b-ajhs-game-programming-23-24-MajinMusic\img\sky.jpg")
+ground_surface = pygame.image.load("C:\8b-ajhs-game-programming-23-24-MajinMusic\img\ground.jpg")
+
+
 while True: 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
     # Draw all our elements
+    screen.blit(sky_surface,(100,0))
+    screen.blit(ground_surface, (0,300))
     # Update everything
     pygame.display.update()
     clock.tick(60)
