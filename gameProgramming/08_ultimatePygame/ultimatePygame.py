f96f93abbe8fb366965c6c4072b5b44fa6fb9e0c
MajinMusic <jacobhyper101@gmail.com>
2024-02-05 14:31:48 -0500

    ultiatePygame.py v0.5
    Created a character sprite on the screen, created rectangles for both snail1.png and player.png.

--- gameProgramming/08_ultimatePygame/ultimatePygame.py
+++ gameProgramming/08_ultimatePygame/ultimatePygame.py
@@ -1,5 +1,4 @@
-# The Ultimate Pygame by Jacob Desha v0.4
-
+# The Ultimate Pygame by Jacob Desha v0.5
 import pygame
 from sys import exit
 
@@ -10,25 +9,33 @@ clock = pygame.time.Clock()
 test_font = pygame.font.Font(None, 50) #(Font type, font size)
 
 
-sky_surface = pygame.image.load("C:\8b-ajhs-game-programming-23-24-MajinMusic\img\sky.jpg")
-ground_surface = pygame.image.load("C:\8b-ajhs-game-programming-23-24-MajinMusic\img\ground.jpg")
+sky_surface = pygame.image.load("C:\8b-ajhs-game-programming-23-24-MajinMusic\img\sky1.png").convert()
+ground_surface = pygame.image.load("C:\8b-ajhs-game-programming-23-24-MajinMusic\img\ground.png").convert()
 text_surface = test_font.render('My game', False, 'Blue') #(text, AA, color)
 
-snail_surface = pygame.image.load("C:\8b-ajhs-game-programming-23-24-MajinMusic\img\download.png")
-snail_x_pos = 600
+snail_surface = pygame.image.load("C:\8b-ajhs-game-programming-23-24-MajinMusic\img\snail1.png").convert_alpha()
+snail_rect = snail_surface.get_rect(bottomright = (0, 375))
 
+player_surface = pygame.image.load("C:\8b-ajhs-game-programming-23-24-MajinMusic\img\player.png").convert_alpha()
+player_rect = player_surface.get_rect(midbottom = (50, 390))
 while True: 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
     # Draw all our elements
-    screen.blit(sky_surface,(100,0))
-    screen.blit(ground_surface, (50,300))
+    screen.blit(sky_surface,(0,0))
+    screen.blit(ground_surface, (-250, 0))
     screen.blit(text_surface, (300, 50))
-    snail_x_pos -= 4
-    if snail_x_pos < -100: snail_x_pos = 800
-    screen.blit(snail_surface, (snail_x_pos, 200))
+    screen.blit(player_surface, player_rect)
+    screen.blit(snail_surface, snail_rect)
+
+    snail_rect.x -= 4
+    if snail_rect.right <= 0: snail_rect.left = 800
+
+    if player_rect.colliderect(snail_rect):
+        print('collision')
+
     # Update everything
     pygame.display.update()
     clock.tick(60)
