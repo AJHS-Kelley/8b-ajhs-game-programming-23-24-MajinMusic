530602553f166b4f54df6bd50c49d6d6475e4b75
MajinMusic <jacobhyper101@gmail.com>
2024-02-18 16:46:19 -0500

    ultimatePygame.py
    Made surfaces look neater, created introduction screen, and displayed score once game is over.

--- gameProgramming/08_ultimatePygame/ultimatePygame.py
+++ gameProgramming/08_ultimatePygame/ultimatePygame.py
@@ -1,20 +1,25 @@
-# The Ultimate Pygame by Jacob Desha v0.11
+# The Ultimate Pygame by Jacob Desha v0.12
 import pygame
 from sys import exit
 
+
+
 def display_score():
     current_time = int(pygame.time.get_ticks() / 1000) - start_time
-    score_surf = test_font.render(f'{current_time}',False,(64,64,64))
+    score_surf = font.render(f'Score: {current_time}',False,(64,64,64))
     score_rect = score_surf.get_rect(center = (400, 50))
     screen.blit(score_surf,score_rect)
+    return current_time
 
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Game')
 clock = pygame.time.Clock()
-test_font = pygame.font.Font(None, 50) #(Font type, font size)
-game_active = True
+font = pygame.font.Font(None, 50) #(Font type, font size)
+start = pygame.font.Font(None, 20)
+game_active = False
 start_time = 0
+score = 0
 # mouse_pos = pygame.mouse.get_pos()
 
 
@@ -25,11 +30,23 @@ ground_surf = pygame.image.load("img\ground.png").convert()
 # score_rect = score_surf.get_rect(midbottom = (400, 50))
 
 snail_surf = pygame.image.load("img\snail1.png").convert_alpha()
+snail_surf = pygame.transform.rotozoom(snail_surf, 0, 0.5)
 snail_rect = snail_surf.get_rect(bottomright = (0, 375))
 
 player_surf = pygame.image.load("img\player.png").convert_alpha()
+player_surf = pygame.transform.rotozoom(player_surf, 0, 0.75)
 player_rect = player_surf.get_rect(midbottom = (50, 390))
 player_gravity = 0
+
+gameName_surf = font.render('Snail Jumper',False, (111,196,69))
+rules_surf = font.render('Press the Spacebar to jump and start!', False, (111,196,69))
+gameName_rect = gameName_surf.get_rect(center = (400, 50))
+rules_rect = rules_surf.get_rect(center = (400, 350))
+
+# Intro screen
+player_stand = pygame.image.load("img\player.png").convert_alpha()
+player_stand = pygame.transform.rotozoom(player_stand, 0, 0.75)
+player_stand_rect = player_stand.get_rect(center = (400, 200))
 
 while True: 
     mouse_pos = pygame.mouse.get_pos()
@@ -66,7 +83,7 @@ while True:
         pygame.draw.ellipse(screen, 'Brown', pygame.Rect(50, 200, 100, 100))
         # pygame.draw.line(screen, 'Gold', (0,0), (800, 400), 10)
         # screen.blit(score_surf, score_rect)
-        display_score()
+        score = display_score()
 
         snail_rect.x -= 4
         if snail_rect.right <= 0: snail_rect.left = 800
@@ -82,7 +99,15 @@ while True:
         if snail_rect.colliderect(player_rect):
             game_active = False
     else:
-        screen.fill('Yellow')
+        screen.fill((94,129,162))
+        screen.blit(player_stand, player_stand_rect)
+
+        score_message = font.render(f'Your score: {score}', False, (111,196,69))
+        score_message_rect = score_message.get_rect(center = (400,330))
+        screen.blit(gameName_surf,gameName_rect)
+
+        if score == 0: screen.blit(rules_surf,rules_rect)
+        else: screen.blit(score_message, score_message_rect)
         
 
 
