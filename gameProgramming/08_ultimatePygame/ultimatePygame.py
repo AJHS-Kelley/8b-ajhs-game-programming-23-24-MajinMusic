bc420cff6be48867bf4e218ac62e2b2e5bc79568
MajinMusic <jacobhyper101@gmail.com>
2024-02-18 17:47:50 -0500

    ultimatePygame.py v0.13
    Created better obstacle spawning and more obstacles.

--- gameProgramming/08_ultimatePygame/ultimatePygame.py
+++ gameProgramming/08_ultimatePygame/ultimatePygame.py
@@ -1,6 +1,7 @@
-# The Ultimate Pygame by Jacob Desha v0.12
+# The Ultimate Pygame by Jacob Desha v0.13
 import pygame
 from sys import exit
+from random import randint
 
 
 
@@ -10,6 +11,25 @@ def display_score():
     score_rect = score_surf.get_rect(center = (400, 50))
     screen.blit(score_surf,score_rect)
     return current_time
+
+def obstacle_movement(obstacle_list):
+    if obstacle_list:
+        for obstacle_rect in obstacle_list:
+            obstacle_rect.x -= 5
+
+            if obstacle_rect.bottom == 375: screen.blit(snail_surf,obstacle_rect)
+            else: screen.blit(bird_surf,obstacle_rect)
+
+        obstacle_list = [obstacle for obstacle in obstacle_list if obstacle.x > 0]
+
+        return obstacle_list
+    else: return []
+
+def collisions(player,obstacles):
+    if obstacles:
+        for obstacle_rect in obstacles:
+            if player.colliderect(obstacle_rect): return False
+    return True
 
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
@@ -23,17 +43,23 @@ score = 0
 # mouse_pos = pygame.mouse.get_pos()
 
 
-sky_surf = pygame.image.load("img\sky1.png").convert()
-ground_surf = pygame.image.load("img\ground.png").convert()
+sky_surf = pygame.image.load("img/sky1.png").convert()
+ground_surf = pygame.image.load("img/ground.png").convert()
 
 # score_surf = test_font.render('My game', False, (64, 64, 64)) #(te)xt, AA, color)
 # score_rect = score_surf.get_rect(midbottom = (400, 50))
 
-snail_surf = pygame.image.load("img\snail1.png").convert_alpha()
-snail_surf = pygame.transform.rotozoom(snail_surf, 0, 0.5)
-snail_rect = snail_surf.get_rect(bottomright = (0, 375))
+# Obstacles
+snail_surf = pygame.image.load("img/snail1.png").convert_alpha()
+snail_surf = pygame.transform.rotozoom(snail_surf, 0, 0.25)
+bird_surf = pygame.image.load("img/bird.png").convert_alpha()
+bird_surf = pygame.transform.rotozoom(bird_surf, 0, 0.075)
+bird_surf = pygame.transform.flip(bird_surf, 180, 0)
 
-player_surf = pygame.image.load("img\player.png").convert_alpha()
+
+obstacle_rect_list = []
+
+player_surf = pygame.image.load("img/player.png").convert_alpha()
 player_surf = pygame.transform.rotozoom(player_surf, 0, 0.75)
 player_rect = player_surf.get_rect(midbottom = (50, 390))
 player_gravity = 0
@@ -44,9 +70,13 @@ gameName_rect = gameName_surf.get_rect(center = (400, 50))
 rules_rect = rules_surf.get_rect(center = (400, 350))
 
 # Intro screen
-player_stand = pygame.image.load("img\player.png").convert_alpha()
+player_stand = pygame.image.load("img/player.png").convert_alpha()
 player_stand = pygame.transform.rotozoom(player_stand, 0, 0.75)
 player_stand_rect = player_stand.get_rect(center = (400, 200))
+
+# Timer
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer,900)
 
 while True: 
     mouse_pos = pygame.mouse.get_pos()
@@ -67,9 +97,13 @@ while True:
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
-                snail_rect.left = 800
                 start_time = int(pygame.time.get_ticks() / 1000)
-        
+
+        if event.type == obstacle_timer and game_active:
+            if randint(0,2):
+                obstacle_rect_list.append(snail_surf.get_rect(bottomright = (randint(900,1100), 375)))
+            else:
+                obstacle_rect_list.append(bird_surf.get_rect(bottomright = (randint(900,1100), 125)))
             
 
 
@@ -85,9 +119,9 @@ while True:
         # screen.blit(score_surf, score_rect)
         score = display_score()
 
-        snail_rect.x -= 4
-        if snail_rect.right <= 0: snail_rect.left = 800
-        screen.blit(snail_surf, snail_rect)
+        # snail_rect.x -= 4
+        # if snail_rect.right <= 0: snail_rect.left = 800
+        # screen.blit(snail_surf, snail_rect)
 
         # Player
         player_gravity += 1
@@ -95,12 +129,18 @@ while True:
         if player_rect.bottom >= 380: player_rect.bottom = 380
         screen.blit(player_surf, player_rect)
 
+        # Obstacle movement
+        obstacle_rect_list = obstacle_movement(obstacle_rect_list)
+
         # collision
-        if snail_rect.colliderect(player_rect):
-            game_active = False
+        game_active = collisions(player_rect,obstacle_rect_list)
+        
     else:
         screen.fill((94,129,162))
         screen.blit(player_stand, player_stand_rect)
+        obstacle_rect_list.clear()
+        player_rect.midbottom = (80,300)
+        player_gravity = 0
 
         score_message = font.render(f'Your score: {score}', False, (111,196,69))
         score_message_rect = score_message.get_rect(center = (400,330))
