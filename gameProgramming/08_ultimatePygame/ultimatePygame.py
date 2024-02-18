3b99f47c44e5e5e39d889ffa83d3d714b9f0b368
MajinMusic <jacobhyper101@gmail.com>
2024-02-18 15:34:42 -0500

    ultimatePygame.py v0.10
    Created game over state and active game state.

--- gameProgramming/08_ultimatePygame/ultimatePygame.py
+++ gameProgramming/08_ultimatePygame/ultimatePygame.py
@@ -7,6 +7,7 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Game')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font(None, 50) #(Font type, font size)
+game_active = True
 # mouse_pos = pygame.mouse.get_pos()
 
 
@@ -27,33 +28,52 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if player_rect.collidepoint(event.pos): 
-                player_gravity = -20
+        
+        if game_active:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if player_rect.collidepoint(event.pos): 
+                    player_gravity = -20
+                
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    player_gravity = -20
+
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                snail_rect.left = 800
+        
             
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                player_gravity = -20
+
 
 # (300, 50)
-    # Draw all our elements
-    screen.blit(sky_surf,(0,0))
-    screen.blit(ground_surf, (-250, 0))
-    pygame.draw.rect(screen, '#c0e8ec', score_rect,)
-    pygame.draw.rect(screen, '#c0e8ec', score_rect, 20)
-    pygame.draw.ellipse(screen, 'Brown', pygame.Rect(50, 200, 100, 100))
-    # pygame.draw.line(screen, 'Gold', (0,0), (800, 400), 10)
-    screen.blit(score_surf, score_rect)
+    if game_active:
+        # Draw all our elements
+        screen.blit(sky_surf,(0,0))
+        screen.blit(ground_surf, (-250, 0))
+        pygame.draw.rect(screen, '#c0e8ec', score_rect,)
+        pygame.draw.rect(screen, '#c0e8ec', score_rect, 20)
+        pygame.draw.ellipse(screen, 'Brown', pygame.Rect(50, 200, 100, 100))
+        # pygame.draw.line(screen, 'Gold', (0,0), (800, 400), 10)
+        screen.blit(score_surf, score_rect)
 
-    snail_rect.x -= 4
-    if snail_rect.right <= 0: snail_rect.left = 800
-    screen.blit(snail_surf, snail_rect)
+        snail_rect.x -= 4
+        if snail_rect.right <= 0: snail_rect.left = 800
+        screen.blit(snail_surf, snail_rect)
 
-    # Player
-    player_gravity += 1
-    player_rect.y += player_gravity
-    if player_rect.bottom >= 380: player_rect.bottom = 380
-    screen.blit(player_surf, player_rect)
+        # Player
+        player_gravity += 1
+        player_rect.y += player_gravity
+        if player_rect.bottom >= 380: player_rect.bottom = 380
+        screen.blit(player_surf, player_rect)
+
+        # collision
+        if snail_rect.colliderect(player_rect):
+            game_active = False
+    else:
+        screen.fill('Yellow')
+        
+
 
 
 
