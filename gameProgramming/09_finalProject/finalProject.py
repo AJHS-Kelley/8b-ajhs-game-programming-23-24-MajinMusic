1274ba0995ef6caddccb907eaaded53ea1155cd3
MajinMusic <jacobhyper101@gmail.com>
2024-03-27 14:48:28 -0400

    finalProject.py
    Made a difficulty option after making variables.

--- gameProgramming/09_finalProject/finalProject.py
+++ gameProgramming/09_finalProject/finalProject.py
@@ -1,1 +1,30 @@
 # Final Project, Jacob Desha, v0.0
+import sys, random, pygame
+
+resolution = 0 # = Low Resolution (800, 600), 1 = High Resolution (1920, 1080)
+x = 0
+y = 0
+
+if resolution == 0:
+    x = 800
+    y = 600
+else:
+    x = 1920
+    y = 1080
+
+pygame.init()
+
+difficulty = int(input("Please choose a difficulty. Enter 1 for EASY or 2 for HARD.\n"))
+
+
+if difficulty == 1:
+    pygame.display.set_caption('HITBOX\n Easy Mode')
+else:
+    pygame.display.set_caption('HITBOX\n Hard Mode')
+
+screen = pygame.display.set_mode((x, y))
+    
+
+
+
+
