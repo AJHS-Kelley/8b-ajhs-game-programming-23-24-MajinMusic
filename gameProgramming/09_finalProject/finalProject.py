30e1d179e8c5881ec3b858cb0e2ede2bda745663
MajinMusic <jacobhyper101@gmail.com>
2024-04-01 14:24:29 -0400

    finalProject.py v0.0
    Created screen, clock, and test font.

--- gameProgramming/09_finalProject/finalProject.py
+++ gameProgramming/09_finalProject/finalProject.py
@@ -1,5 +1,7 @@
-# Final Project, Jacob Desha, v0.0
-import sys, random, pygame
+# Final Project, Jacob Desha, v0.1
+import pygame
+from sys import exit 
+import random
 
 resolution = 0 # = Low Resolution (800, 600), 1 = High Resolution (1920, 1080)
 x = 0
@@ -13,18 +15,24 @@ else:
     y = 1080
 
 pygame.init()
+pygame.display.set_caption('game')
+clock = pygame.time.Clock()
+font = pygame.font.Font(None, 50) 
 
 difficulty = int(input("Please choose a difficulty. Enter 1 for EASY or 2 for HARD.\n"))
 
-
 if difficulty == 1:
-    pygame.display.set_caption('HITBOX\n Easy Mode')
+    pygame.display.set_caption('HITBOX: Easy Mode')
 else:
-    pygame.display.set_caption('HITBOX\n Hard Mode')
+    pygame.display.set_caption('HITBOX: Hard Mode')
 
 screen = pygame.display.set_mode((x, y))
-    
 
-
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    pygame.display.update()
 
 
