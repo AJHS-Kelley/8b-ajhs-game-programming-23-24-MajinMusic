a16b434fb190a2087320e54ecb1216d31a367e00
MajinMusic <jacobhyper101@gmail.com>
2024-04-01 14:08:07 -0400

    abc.py
    Completed Problem.

--- gameProgramming/10_kattis/abc.py
+++ gameProgramming/10_kattis/abc.py
@@ -12,11 +12,29 @@ c = int(c)
 
 print(f"a: {a} b: {b} c: {c}")
 while True:
-    if a >= b:
-        a, b = b, a
     if b >= c:
         b, c = c, b
-    else: 
+    if a >= b:
+        a, b = b, a
+    if a <= b <= c:
         break
-
 print(f"a: {a} b: {b} c: {c}")
+
+# input the string variable
+# determine order of A, B, and C
+# create correct string
+# output string
+
+order = input()
+myString = ""
+
+for i in range(len(order)):
+    if order[i] == "A":
+        myString += str(a) + " "
+    elif order[i] == "B":
+        myString += str(b) + " "
+    else:
+        myString += str(c) + " "
+
+print(myString)
+
