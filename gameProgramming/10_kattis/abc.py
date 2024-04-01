151dfb4254daaa62b324242d0b0ea48fd2e74866
MajinMusic <jacobhyper101@gmail.com>
2024-04-01 13:52:57 -0400

    abc.py
    Created variable integers and statement to put variables in order from least to greatest.

---
+++ gameProgramming/10_kattis/abc.py
@@ -0,0 +1,22 @@
+# input the integers
+# .split() the 3 integers into separate variables.
+# cast the 3 variables to integers (Changing one datatype to another).
+# assign correct values from least to greatest
+# A < B < C
+
+integers = input()
+a, b, c = integers.split()
+a = int(a)
+b = int(b)
+c = int(c)
+
+print(f"a: {a} b: {b} c: {c}")
+while True:
+    if a >= b:
+        a, b = b, a
+    if b >= c:
+        b, c = c, b
+    else: 
+        break
+
+print(f"a: {a} b: {b} c: {c}")
